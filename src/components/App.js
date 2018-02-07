c735617461142ff9a09331fd208a3fc5bd3b3104
Lisa Lee <snickers495@gmail.com>
2018-02-07 16:55:20 +0000

    automatically backed up by learn

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import FruitBasket from './FruitBasket';
 
-export default class App extends  <FruitBasket />;
+export default class App extends React.Component {
+  
+} <FruitBasket />;
 
 export default App;
