43518fe0a31e81cdd4ff7a9527ab19c7b813a501
Lisa Lee <snickers495@gmail.com>
2018-02-07 17:06:05 +0000

    automatically backed up by learn

--- src/components/FruitBasket.js
+++ src/components/FruitBasket.js
@@ -4,7 +4,7 @@ import Filter from './Filter';
 import FilteredFruitList from './FilteredFruitList.js';
 
 const FruitBasket = (props) => {
-  const filters = props.filters
+  const filters = props.state.filters
   const selectedFilter = props.selectedFilter
   const handleFilterChange = props.handleFilterChange
   return (
