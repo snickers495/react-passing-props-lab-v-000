1b13dfd003e531fbf6f6d15aec2320479987760b
Lisa Lee <snickers495@gmail.com>
2018-02-07 17:03:15 +0000

    automatically backed up by learn

--- src/components/FilteredFruitList.js
+++ src/components/FilteredFruitList.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 const FilteredFruitList = (props)=> {
   const items = props.items
   const propFilter = props.filter
+  const list = !propFilter || propFilter === 'all' ? items : items.filter(i => i.fruit_type === propFilter);
+
   componentDidMount() {
     fetch('/api/fruit')
       .then(response => response.json())
@@ -15,7 +17,6 @@ const FilteredFruitList = (props)=> {
         {list.map((item, index) => <li key={index}>{item.char}</li>)}
       </ul>
     );
-  }
 }
 
 export default FilteredFruitList;
