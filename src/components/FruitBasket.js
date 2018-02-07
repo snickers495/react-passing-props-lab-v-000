d13c5b4230e00db0c76ec49e37b8d9fe604540a0
Lisa Lee <snickers495@gmail.com>
2018-02-07 17:06:36 +0000

    automatically backed up by learn

--- src/components/FruitBasket.js
+++ src/components/FruitBasket.js
@@ -11,7 +11,7 @@ const FruitBasket = (props) => {
     <div className="fruit-basket">
       <Filter handleChange={handleFilterChange} />
       <FilteredFruitList
-        filter={selectedFilter} filters=/>
+        filter={selectedFilter} filters={filters}/>
     </div>
   );
 
