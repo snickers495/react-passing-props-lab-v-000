6924c0d97894cd2d88514a36c6c3a782c65057bd
Lisa Lee <snickers495@gmail.com>
2018-02-07 17:02:46 +0000

    automatically backed up by learn

--- src/components/FilteredFruitList.js
+++ src/components/FilteredFruitList.js
@@ -10,7 +10,7 @@ const FilteredFruitList = (props)=> {
   }
 
   render() {
-    const list = !propFilter || ilter === 'all' ? items : items.filter(i => i.fruit_type === filter);
+    const list = !propFilter || propFilter === 'all' ? items : items.filter(i => i.fruit_type === filter);
 
     return (
       <ul className="fruit-list">
