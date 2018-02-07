0b1a94e77673f9022217fa46ce5373550d3fb9e5
Lisa Lee <snickers495@gmail.com>
2018-02-07 17:07:05 +0000

    automatically backed up by learn

--- src/components/FilteredFruitList.js
+++ src/components/FilteredFruitList.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 
 const FilteredFruitList = (props)=> {
   const items = props.items
-  const propFilter = props.filter
+  const propFilter = props.selectedFilterfilter
   const list = !propFilter || propFilter === 'all' ? items : items.filter(i => i.fruit_type === propFilter);
 
-  
     return (
       <ul className="fruit-list">
         {list.map((item, index) => <li key={index}>{item.char}</li>)}
