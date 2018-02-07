b9b5b75511cb234ae348f35f6a34868f643141b5
Lisa Lee <snickers495@gmail.com>
2018-02-07 17:05:10 +0000

    automatically backed up by learn

--- src/components/App.js
+++ src/components/App.js
@@ -28,7 +28,7 @@ export default class App extends React.Component {
 
   render(){
     return(
-      <FruitBasket handleFilterChange={this.handleFilterChange}/>;
+      <FruitBasket handleFilterChange={this.handleFilterChange} fetchFilters/>;
     )
   }
 }
