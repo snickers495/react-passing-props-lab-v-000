5b8c471552adcf47aa006e24490601c4231ae1cd
Lisa Lee <snickers495@gmail.com>
2018-02-07 17:09:09 +0000

    automatically backed up by learn

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 const Filter = (props) => {
   const filter = props.filter;
   const handleChange = props.handleChange
+  const filters = props.filters
 
 
     return (
